1d93b0eedc071f733e539c94fca86b3611518770
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-11-23 12:17:46 +0200

    Update configuration to work with the latest `ecs` version

--- ecs.php
+++ ecs.php
@@ -7,7 +7,6 @@ use PhpCsFixer\Fixer\ArrayNotation\NoMultilineWhitespaceAroundDoubleArrowFixer;
 use PhpCsFixer\Fixer\ArrayNotation\NormalizeIndexBraceFixer;
 use PhpCsFixer\Fixer\ArrayNotation\NoTrailingCommaInSinglelineArrayFixer;
 use PhpCsFixer\Fixer\ArrayNotation\NoWhitespaceBeforeCommaInArrayFixer;
-use PhpCsFixer\Fixer\ArrayNotation\TrailingCommaInMultilineArrayFixer;
 use PhpCsFixer\Fixer\ArrayNotation\WhitespaceAfterCommaInArrayFixer;
 use PhpCsFixer\Fixer\Basic\BracesFixer;
 use PhpCsFixer\Fixer\CastNotation\LowercaseCastFixer;
@@ -17,6 +16,7 @@ use PhpCsFixer\Fixer\ClassNotation\ProtectedToPrivateFixer;
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use PhpCsFixer\Fixer\ControlStructure\NoUnneededControlParenthesesFixer;
 use PhpCsFixer\Fixer\ControlStructure\NoUnneededCurlyBracesFixer;
+use PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer;
 use PhpCsFixer\Fixer\FunctionNotation\ReturnTypeDeclarationFixer;
 use PhpCsFixer\Fixer\FunctionNotation\VoidReturnFixer;
 use PhpCsFixer\Fixer\Import\NoLeadingImportSlashFixer;
@@ -37,116 +37,109 @@ use PhpCsFixer\Fixer\Semicolon\NoSinglelineWhitespaceBeforeSemicolonsFixer;
 use PhpCsFixer\Fixer\Whitespace\CompactNullableTypehintFixer;
 use PhpCsFixer\Fixer\Whitespace\HeredocIndentationFixer;
 use PhpCsFixer\Fixer\Whitespace\NoTrailingWhitespaceFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\Commenting\ParamReturnAndVarTagMalformsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 /*
  * @see https://github.com/symplify/easy-coding-standard/blob/master/config/set/psr12.php
  */
-$psr12 = static function (ContainerConfigurator $config): void {
-    $services = $config->services();
+$psr12 = static function (ECSConfig $config): void {
+    $config->rule(LowercaseCastFixer::class);
+    $config->rule(ShortScalarCastFixer::class);
+    $config->rule(BlankLineAfterOpeningTagFixer::class);
+    $config->rule(NoLeadingImportSlashFixer::class);
+    $config->ruleWithConfiguration(OrderedImportsFixer::class, [
+        'importsOrder' => ['class', 'function', 'const'],
+    ]);
 
-    $services->set(LowercaseCastFixer::class);
-    $services->set(ShortScalarCastFixer::class);
-    $services->set(BlankLineAfterOpeningTagFixer::class);
-    $services->set(NoLeadingImportSlashFixer::class);
-    $services->set(OrderedImportsFixer::class)
-        ->call('configure', [
-            [
-                'importsOrder' => ['class', 'function', 'const'],
-            ],
-        ]);
+    $config->ruleWithConfiguration(DeclareEqualNormalizeFixer::class, [
+        'space' => 'none',
+    ]);
 
-    $services->set(DeclareEqualNormalizeFixer::class)->call('configure', [['space' => 'none']]);
+    $config->rule(NewWithBracesFixer::class);
+    $config->ruleWithConfiguration(BracesFixer::class, [
+        'allow_single_line_closure'                   => false,
+        'position_after_functions_and_oop_constructs' => 'next',
+        'position_after_control_structures'           => 'same',
+        'position_after_anonymous_constructs'         => 'same',
+    ]);
 
-    $services->set(NewWithBracesFixer::class);
-    $services->set(BracesFixer::class)
-        ->call('configure', [
-            [
-                'allow_single_line_closure'                   => false,
-                'position_after_functions_and_oop_constructs' => 'next',
-                'position_after_control_structures'           => 'same',
-                'position_after_anonymous_constructs'         => 'same',
-            ],
-        ]);
-
-    $services->set(NoBlankLinesAfterClassOpeningFixer::class);
-    $services->set(VisibilityRequiredFixer::class)
-        ->call('configure', [
-            [
-                'elements' => ['const', 'method', 'property'],
-            ],
-        ]);
-    $services->set(BinaryOperatorSpacesFixer::class);
-    $services->set(TernaryOperatorSpacesFixer::class);
-    $services->set(UnaryOperatorSpacesFixer::class);
-    $services->set(ReturnTypeDeclarationFixer::class);
-    $services->set(NoTrailingWhitespaceFixer::class);
-    $services->set(ConcatSpaceFixer::class)->call('configure', [['spacing' => 'one']]);
-    $services->set(NoSinglelineWhitespaceBeforeSemicolonsFixer::class);
-    $services->set(NoWhitespaceBeforeCommaInArrayFixer::class);
-    $services->set(WhitespaceAfterCommaInArrayFixer::class);
+    $config->rule(NoBlankLinesAfterClassOpeningFixer::class);
+    $config->ruleWithConfiguration(VisibilityRequiredFixer::class, [
+        'elements' => ['const', 'method', 'property'],
+    ]);
+    $config->rule(BinaryOperatorSpacesFixer::class);
+    $config->rule(TernaryOperatorSpacesFixer::class);
+    $config->rule(UnaryOperatorSpacesFixer::class);
+    $config->rule(ReturnTypeDeclarationFixer::class);
+    $config->rule(NoTrailingWhitespaceFixer::class);
+    $config->ruleWithConfiguration(ConcatSpaceFixer::class, [
+        'spacing' => 'one',
+    ]);
+    $config->rule(NoSinglelineWhitespaceBeforeSemicolonsFixer::class);
+    $config->rule(NoWhitespaceBeforeCommaInArrayFixer::class);
+    $config->rule(WhitespaceAfterCommaInArrayFixer::class);
 };
 
 /*
  * @see https://github.com/symplify/easy-coding-standard/blob/master/config/set/php71.php
  */
-$php71 = static function (ContainerConfigurator $config): void {
+$php71 = static function (ECSConfig $config): void {
     $services = $config->services();
 
-    $services->set(VisibilityRequiredFixer::class)
-        ->call('configure', [
-            [
-                'elements' => ['const', 'property', 'method'],
-            ],
-        ]);
+    $config->ruleWithConfiguration(VisibilityRequiredFixer::class, [
+        'elements' => ['const', 'property', 'method'],
+    ]);
 
-    $services->set(ListSyntaxFixer::class)->call('configure', [['syntax' => 'short']]);
-    $services->set(CompactNullableTypehintFixer::class);
-    $services->set(VoidReturnFixer::class);
+    $config->ruleWithConfiguration(ListSyntaxFixer::class, [
+        'syntax' => 'short',
+    ]);
+    $config->rule(CompactNullableTypehintFixer::class);
+    $config->rule(VoidReturnFixer::class);
 };
 
 /*
  * @see https://github.com/symplify/easy-coding-standard/blob/master/config/set/clean-code.php
  */
-$cleanCode = static function (ContainerConfigurator $config): void {
+$cleanCode = static function (ECSConfig $config): void {
     $services = $config->services();
 
-    $services->set(ParamReturnAndVarTagMalformsFixer::class);
-    $services->set(ArraySyntaxFixer::class)->call('configure', [['syntax' => 'short']]);
-    $services->set(NoUnusedImportsFixer::class);
-    $services->set(OrderedImportsFixer::class)->call('configure',
-        [['imports_order' => ['class', 'function', 'const']]]);
-    $services->set(NoEmptyStatementFixer::class);
-    $services->set(ProtectedToPrivateFixer::class);
-    $services->set(NoUnneededControlParenthesesFixer::class);
-    $services->set(NoUnneededCurlyBracesFixer::class);
-    $services->set(ReturnAssignmentFixer::class);
+    $config->rule(ParamReturnAndVarTagMalformsFixer::class);
+    $config->ruleWithConfiguration(ArraySyntaxFixer::class, [
+        'syntax' => 'short',
+    ]);
+    $config->rule(NoUnusedImportsFixer::class);
+    $config->ruleWithConfiguration(OrderedImportsFixer::class, [
+        'imports_order' => ['class', 'function', 'const'],
+    ]);
+    $config->rule(NoEmptyStatementFixer::class);
+    $config->rule(ProtectedToPrivateFixer::class);
+    $config->rule(NoUnneededControlParenthesesFixer::class);
+    $config->rule(NoUnneededCurlyBracesFixer::class);
+    $config->rule(ReturnAssignmentFixer::class);
 };
 
-return static function (ContainerConfigurator $config) use ($psr12, $cleanCode, $php71): void {
+return static function (ECSConfig $config) use ($psr12, $cleanCode, $php71): void {
     $psr12($config);
     $php71($config);
     $cleanCode($config);
 
-    $services = $config->services();
-
-    $services->set(NotOperatorWithSuccessorSpaceFixer::class);
-    $services->set(NoMultilineWhitespaceAroundDoubleArrowFixer::class);
-    $services->set(NormalizeIndexBraceFixer::class);
-    $services->set(NoTrailingCommaInSinglelineArrayFixer::class);
-    $services->set(NoWhitespaceBeforeCommaInArrayFixer::class);
-    $services->set(TrailingCommaInMultilineArrayFixer::class);
-    $services->set(WhitespaceAfterCommaInArrayFixer::class);
+    $config->rule(NotOperatorWithSuccessorSpaceFixer::class);
+    $config->rule(NoMultilineWhitespaceAroundDoubleArrowFixer::class);
+    $config->rule(NormalizeIndexBraceFixer::class);
+    $config->rule(NoTrailingCommaInSinglelineArrayFixer::class);
+    $config->rule(NoWhitespaceBeforeCommaInArrayFixer::class);
+    $config->rule(TrailingCommaInMultilineFixer::class);
+    $config->rule(WhitespaceAfterCommaInArrayFixer::class);
 
     // Override PSR12 preset
-    $services->set(BinaryOperatorSpacesFixer::class)->call('configure', [
-        [
-            'operators' => [
-                '=>' => null, // disable aligning double arrow statementsx
-            ],
+    $config->ruleWithConfiguration(BinaryOperatorSpacesFixer::class, [
+        'operators' => [
+            '=>' => null, // disable aligning double arrow statementsx
         ],
     ]);
 
-    $services->set(HeredocIndentationFixer::class)->call('configure', [['indentation' => 'same_as_start']]);
+    $config->ruleWithConfiguration(HeredocIndentationFixer::class, [
+        'indentation' => 'same_as_start',
+    ]);
 };
